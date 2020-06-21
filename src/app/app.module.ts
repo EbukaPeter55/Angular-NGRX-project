6e03538b6018f57218e940dd8ebf60eae663eda8
Ebuka Peter <peterdbrainy5@gmail.com>
2020-06-21 22:36:17 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from "@ngrx/store";
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({}),
     AppRoutingModule
   ],
   providers: [],
