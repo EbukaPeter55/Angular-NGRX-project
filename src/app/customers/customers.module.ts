4cca4156cfb8f0204401101159e1217d22019f9e
Ebuka Peter <peterdbrainy5@gmail.com>
2020-06-24 08:50:58 +0100

    Loaded customers from the store, showed how the actions and reducers work

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -5,13 +5,18 @@ import { CustomerComponent } from './customer/customer.component';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
-
+import { StoreModule } from "@ngrx/store";
+import { customerReducer } from "./state/customer.reducer";
 
 const customerRoutes: Routes = [{ path: '', component: CustomerComponent }];
 
 
 @NgModule({
-   imports: [CommonModule, RouterModule.forChild(customerRoutes)],
+   imports: [
+     CommonModule, 
+     RouterModule.forChild(customerRoutes),
+     StoreModule.forFeature("customers", customerReducer)
+    ],
    declarations: [
      CustomerComponent,
      CustomerAddComponent,
